b8bbe35d76f3bfff2b074ca8563ee8330c79de56
garvinipkiss <garvinipkiss@gmail.com>
2018-03-21 23:02:59 +0300

    perfecting

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -18,6 +18,13 @@ export class GoalComponent implements OnInit {
     toogleDetails(index){
       this.goals[index].showDescription = !this.goals[index].showDescription;
     }
+    addNewGoal(goal){
+       let goalLength = this.goals.length;
+       goal.id=goalLength+1;
+       goal.completeDate = new Date(goal.completeDate)
+       this.goals.push(goal)
+
+   }
     deleteGoal(isComplete,index){
       if (isComplete){
         let toDelete =confirm(`Are you sure you want to delete ${this.goals[index].name}`)
